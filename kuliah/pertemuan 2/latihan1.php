026d81476ce11332c381468d648152cb4e41c1ff
RizkyPM40076 <rizky.213040097@mail.unpas.ac.id>
2022-03-05 11:52:44 +0700

    Pertemuan 2

---
+++ kuliah/pertemuan 2/latihan1.php
@@ -0,0 +1,32 @@
+<?php 
+//pertemuan 2. belajar sintaks PHP
+
+//nilai
+//angka(integer),tulisan(string), true/false(boolean)
+echo 10 ; //integer
+echo '<br>';
+echo 'daffa'; //string
+echo false;
+echo '<br>';
+$nama_depan = 'Rizky';
+$nama_belakang = 'Priya Mandiri';
+echo 'sandhika: "selamat hari jum\'at"';
+echo '<br>';
+echo $nama_depan . " " . $nama_belakang ;
+echo'<br>';
+
+// operator
+// aritmatika 
+// +,-,*,/,%
+echo 1 + 1; //Hasil dari 1+1 adalah 2;
+echo '<br>';
+echo 1 + 2 * 3 - 4;
+echo 10 % 5 ;
+
+
+// perbandingan
+// <,>,<=,>=,==,!=
+
+
+
+?>
